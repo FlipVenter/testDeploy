2bf88a7c27c61f6b96670283101a5567d2ee30fb
FlipVenter <119102066+FlipVenter@users.noreply.github.com>
2025-03-11 06:55:58 +0200

    some responsive

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -47,7 +47,7 @@ class Login extends React.Component {
 
     render() {
         return (
-            <div className="flex flex-col justify-start items-center box-border h-[90vh] w-full">
+            <div className="flex flex-col justify-start items-center box-border h-[90vh] w-full sm:overflow-y-scroll">
                 <div className="relative flex flex-row justify-center items-center gap-[3vw] box-border bg-myBlue-300 w-full h-[80vh]">
                     <div className="absolute inset-0 bg-cover bg-center opacity-20" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
                     <div className="relative flex flex-col justify-start items-center box-border lg:text-7xl xl:text-8xl text-white w-[50vw] h-5/6 md:hidden ">
