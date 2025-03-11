bd9c10fee243b89632f8cb41012accec1d3c4478
FlipVenter <119102066+FlipVenter@users.noreply.github.com>
2025-03-11 13:21:03 +0200

    some testing

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -91,50 +91,57 @@ class Login extends React.Component {
             //         </div>
             //     </div>
             // </div>
-            <div className="flex flex-col justify-start items-center box-border h-full w-full overflow-x-hidden">
-            <div className="relative flex flex-row justify-center items-center gap-[3vw] box-border bg-myBlue-300 w-full h-[90vh] xl:h-[80vh]">
-                <div className="absolute inset-0 bg-cover bg-center opacity-20" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
-                <div className="relative hidden xl:flex flex-col justify-start items-center box-border lg:text-7xl xl:text-8xl text-white w-[50vw] h-5/6">
-                        Estate Liquidity Calculator
-                </div>
-                <div className="relative flex flex-col justify-center items-center box-border rounded-xl bg-white xl:w-[35vw] h-5/6 p-1 md:p-8 w-screen mx-2">
-                    <form className="h-full w-full flex flex-col justify-start items-center p-0" onSubmit={this.handleSubmit}>
-                            <div className=" rounded-md w-full h-[15%] flex flex-start items-center justify-center text-8xl xl:text-4xl">
-                                Login(under construction)
-                            </div>
-
-                            <div className=" w-full h-[65%] flex flex-col justify-center items-center gap-10 p-3">
-                                    <input
-                                        name="email"
-                                        value={this.state.email}
-                                        onChange={this.handleChange}
-                                        placeholder="Email"
-                                        className="w-full h-[20%] border-none bg-gray-200 rounded-md px-2 py-5  focus:outline-myBlue-100 invalid:outline-red-600 text-5xl md-text-full"
-                                        required
-                                    />
-
-                                    <input
-                                        name="password"
-                                        value={this.state.password}
-                                        onChange={this.handleChange}
-                                        placeholder="Password"
-                                        type="password"
-                                        className="w-full h-[20%] border-none bg-gray-200 rounded-md  px-2 py-5  focus:outline-myBlue-100 invalid:outline-red-500 text-5xl md-text-full"
-                                        required
-                                    />
-                            </div>
-
-                            <div className="flex justify-center items-center  w-full h-[10%]">
-                                <NavLink to="/RegisterUser"className="hover:text-myYellow-100 hover:bg-none text-5xl md:text-full">Don't have an account? </NavLink>
-                            </div>
-
-                            <div className=" w-full h-14 flex flex-col justify-center items-center text-5xl box-border">
-                                <button type="submit" className="bg-myBlue-200 w-fit md:w-[10vw] h-fit text-5xl p-3 text-white">Login</button>
-                            </div>
-                    </form>
+            <div class="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
+                <div class="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
+                    <h1 class="font-bold text-center text-2xl mb-5">Your Logo</h1>  
+                    <div class="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
+                    <div class="px-5 py-7">
+                        <label class="font-semibold text-sm text-gray-600 pb-1 block">E-mail</label>
+                        <input type="text" class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
+                        <label class="font-semibold text-sm text-gray-600 pb-1 block">Password</label>
+                        <input type="text" class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
+                        <button type="button" class="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
+                            <span class="inline-block mr-2">Login</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 inline-block">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="py-5">
+                        <div class="grid grid-cols-2 gap-1">
+                        <div class="text-center sm:text-left whitespace-nowrap">
+                            <button class="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 inline-block align-text-top">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z" />
+                                </svg>
+                                <span class="inline-block ml-1">Forgot Password</span>
+                            </button>
+                        </div>
+                        <div class="text-center sm:text-right  whitespace-nowrap">
+                            <button class="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 inline-block align-text-bottom	">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
+                                </svg>
+                                <span class="inline-block ml-1">Help</span>
+                            </button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="py-5">
+                        <div class="grid grid-cols-2 gap-1">
+                        <div class="text-center sm:text-left whitespace-nowrap">
+                            <button class="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-200 focus:outline-none focus:bg-gray-300 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 inline-block align-text-top">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                                </svg>
+                                <span class="inline-block ml-1">Back to your-app.com</span>
+                            </button>
+                        </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
         )
     }
 }
