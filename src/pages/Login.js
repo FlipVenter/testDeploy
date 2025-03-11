ba565c1dd14f8977f502d065d842f66530303e11
FlipVenter <119102066+FlipVenter@users.noreply.github.com>
2025-03-11 08:27:49 +0200

    R

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -91,13 +91,13 @@ class Login extends React.Component {
             //         </div>
             //     </div>
             // </div>
-            <div className="flex flex-col justify-start items-center box-border h-full w-full overflow-hidden">
+            <div className="flex flex-col justify-start items-center box-border h-full w-screen md:w-full overflow-hidden">
             <div className="relative flex flex-row justify-center items-center gap-[3vw] box-border bg-myBlue-300 w-full h-[80vh]">
                 <div className="absolute inset-0 bg-cover bg-center opacity-20" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
                 <div className="relative hidden flex-col justify-start items-center box-border lg:text-7xl xl:text-8xl text-white w-[50vw] h-5/6">
                         Estate Liquidity Calculator
                 </div>
-                <div className="relative flex flex-col justify-center items-center box-border rounded-xl bg-white w-[90%] sm:w-[35vw] h-5/6 p-1 md:p-8 min-w-[350px]">
+                <div className="relative flex flex-col justify-center items-center box-border rounded-xl bg-white w-[95%] md:w-[35vw] h-5/6 p-1 md:p-8 min-w-[350px]">
                     <form className="h-full w-full flex flex-col justify-start items-center p-0" onSubmit={this.handleSubmit}>
                             <div className=" rounded-md w-full h-[15%] flex flex-start items-center justify-center text-4xl">
                                 Login
@@ -109,7 +109,7 @@ class Login extends React.Component {
                                         value={this.state.email}
                                         onChange={this.handleChange}
                                         placeholder="Email"
-                                        className="w-full h-[20%] border-none bg-gray-200 rounded-md p-2  focus:outline-myBlue-100 invalid:outline-red-600"
+                                        className="w-full h-[20%] border-none bg-gray-200 rounded-md p-2  focus:outline-myBlue-100 invalid:outline-red-600 text-5xl md-text-full"
                                         required
                                     />
 
@@ -119,7 +119,7 @@ class Login extends React.Component {
                                         onChange={this.handleChange}
                                         placeholder="Password"
                                         type="password"
-                                        className="w-full h-[20%] border-none bg-gray-200 rounded-md p-2 focus:outline-myBlue-100 invalid:outline-red-500"
+                                        className="w-full h-[20%] border-none bg-gray-200 rounded-md p-2 focus:outline-myBlue-100 invalid:outline-red-500 text-5xl md-text-full"
                                         required
                                     />
                             </div>
